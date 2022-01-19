bf49c38782e61fa737ddf200bf70b777b334b596
Arthur <arthur@fetch.ly>
2022-01-18 22:24:26 -0300

    finished section 4

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
@@ -45,4 +45,107 @@ test("should be able to type confirm password", () => {
   const confirmPassword = "12345678";
   userEvent.type(confirmPasswordInputElement, confirmPassword);
   expect(confirmPasswordInputElement.value).toBe(confirmPassword);
+});
+
+test("should show email error message on invalid email", () => {
+  render(<App />);
+  const emailErrorMessageElement = screen.queryByText(
+    /the email you put in is invalid/i
+  );
+  const email = "arthurmfgtabgmail.com";
+  const submitBtnElement = screen.getByRole("button", { name: /submit/i });
+
+  expect(emailErrorMessageElement).not.toBeInTheDocument();
+
+  const emailInputElement: HTMLInputElement = screen.getByRole("textbox", {
+    name: /email/i,
+  });
+  userEvent.type(emailInputElement, email);
+  userEvent.click(submitBtnElement);
+
+  const emailErrorMessageElementAgain = screen.queryByText(
+    /the email you put in is invalid/i
+  );
+  expect(emailErrorMessageElementAgain).toBeInTheDocument();
+});
+
+test("should show error if provided password is less than 5 characters", () => {
+  render(<App />);
+
+  const email = "arthurmfgtabgmail.com";
+  const password = "1234";
+  const confirmPassword = "1234";
+
+  const emailInputElement: HTMLInputElement = screen.getByPlaceholderText(
+    "blablabla@example.com"
+  );
+  const passwordInputElement: HTMLInputElement =
+    screen.getByPlaceholderText("password123");
+  const confirmPasswordInputElement: HTMLInputElement =
+    screen.getByPlaceholderText("confirm123");
+
+  userEvent.type(emailInputElement, email);
+  userEvent.type(passwordInputElement, password);
+  userEvent.type(confirmPasswordInputElement, confirmPassword);
+
+  const submitBtnElement = screen.getByRole("button", { name: /submit/i });
+  userEvent.click(submitBtnElement);
+
+  const emailErrorMessageElementAgain = screen.queryByText(
+    /password must be at least 5 characters/i
+  );
+  expect(emailErrorMessageElementAgain).toBeInTheDocument();
+});
+
+test("should show error if passwords are not matching", () => {
+  render(<App />);
+
+  const email = "arthurmfgtabgmail.com";
+  const password = "1234";
+  const confirmPassword = "12344";
+
+  const emailErrorMessageElement1 = screen.queryByText(
+    /mismatching passwords/i
+  );
+  expect(emailErrorMessageElement1).not.toBeInTheDocument();
+
+  const emailInputElement: HTMLInputElement = screen.getByPlaceholderText(
+    "blablabla@example.com"
+  );
+  const passwordInputElement: HTMLInputElement =
+    screen.getByPlaceholderText("password123");
+  const confirmPasswordInputElement: HTMLInputElement =
+    screen.getByPlaceholderText("confirm123");
+
+  expect(emailInputElement.value).toBe("");
+  expect(passwordInputElement.value).toBe("");
+  expect(confirmPasswordInputElement.value).toBe("");
+
+  userEvent.type(emailInputElement, email);
+  const emailInputElementWithValue: HTMLInputElement =
+    screen.getByPlaceholderText("blablabla@example.com");
+  expect(emailInputElementWithValue.value).toBe(email);
+
+  userEvent.type(passwordInputElement, password);
+  const passwordInputElementWithValue: HTMLInputElement =
+    screen.getByPlaceholderText("password123");
+  expect(passwordInputElementWithValue.value).toBe(password);
+
+  userEvent.type(confirmPasswordInputElement, confirmPassword);
+  const confirmPasswordInputElementWithValue: HTMLInputElement =
+    screen.getByPlaceholderText("confirm123");
+  expect(confirmPasswordInputElementWithValue.value).toBe(confirmPassword);
+
+  const emailErrorMessageElement2 = screen.queryByText(
+    /mismatching passwords/i
+  );
+  expect(emailErrorMessageElement2).not.toBeInTheDocument();
+
+  const submitBtnElement = screen.getByRole("button", { name: /submit/i });
+  userEvent.click(submitBtnElement);
+
+  const emailErrorMessageElement3 = screen.queryByText(
+    /mismatching passwords/i
+  );
+  expect(emailErrorMessageElement3).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { ChangeEvent, MouseEventHandler, useState } from "react";
 import "./App.css";
+import validator from "validator";
 
 function App() {
+  const [error, setError] = useState("");
+  const [passwordTooSmallError, setPasswordTooSmallError] = useState("");
+  const [passwordMismatchError, setPasswordMismatchError] = useState("");
+  const [signupInput, setSignupInput] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  function onChange({ target }: ChangeEvent<HTMLInputElement>) {
+    setSignupInput({ ...signupInput, [target.name]: target.value });
+  }
+
+  function handleClick(event: any) {
+    //MouseEventHandler<HTMLButtonElement>) {
+    const { email, password, confirmPassword } = signupInput;
+
+    event.preventDefault();
+
+    if (!validator.isEmail(email)) {
+      setError("The email you put in is invalid");
+    }
+
+    if (password.length < 5) {
+      setPasswordTooSmallError("Password must be at least 5 characters");
+    }
+
+    if (password !== confirmPassword) {
+      setPasswordMismatchError("mismatching passwords");
+    }
+  }
+
   return (
     <div className="container my-5">
       <form>
@@ -14,6 +47,9 @@ function App() {
             id="email"
             name="email"
             className="form-control"
+            value={signupInput.email}
+            onChange={onChange}
+            placeholder="blablabla@example.com"
           />
         </div>
         <div className="mb-3">
@@ -25,6 +61,9 @@ function App() {
             id="password"
             name="password"
             className="form-control"
+            value={signupInput.password}
+            onChange={onChange}
+            placeholder="password123"
           />
         </div>
         <div className="mb-3">
@@ -34,10 +73,25 @@ function App() {
           <input
             type="password"
             id="confirm-password"
-            name="confirm-password"
+            name="confirmPassword"
             className="form-control"
+            value={signupInput.confirmPassword}
+            onChange={onChange}
+            placeholder="confirm123"
           />
         </div>
+
+        {error && <div className="alert alert-danger">{error}</div>}
+        {passwordMismatchError && (
+          <div className="alert alert-danger">{passwordMismatchError}</div>
+        )}
+        {passwordTooSmallError && (
+          <div className="alert alert-danger">{passwordTooSmallError}</div>
+        )}
+
+        <button className="btn btn-primary" onClick={handleClick} type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
